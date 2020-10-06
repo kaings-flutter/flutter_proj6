aca8231e0fa392a3f9be6b31864ca3f2edfac8ac
willys_tjiawi <willys@huubap.com>
2020-10-06 15:00:05 +0800

    FutureBuilder: other alternative without using StatefulWidget (if just want to use loading spinner ONLY, we can just use FutureBuilder without having to convert to StatefulWidget)

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -13,96 +13,72 @@ enum DisplayOptions {
   ShowAll,
 }
 
-class ProductOverviewScreen extends StatefulWidget {
-  @override
-  _ProductOverviewScreenState createState() => _ProductOverviewScreenState();
-}
-
-class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
-  var _showFavoriteOnly = false;
-  var _isInit = true;
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    // Provider.of<Products>(context, listen: false).fetchProducts(); // wont work `context` not avail on initState
-
-    // fyi, this will also work but it is kinda hacky way
-    // Future.delayed(Duration.zero).then((_) {
-    //   Provider.of<Products>(context).fetchProducts();
-    // });
-    super.initState();
-  }
-
-  @override
-  void didChangeDependencies() {
-    if (_isInit) {
-      _isLoading = true;
-      Provider.of<Products>(context).fetchProducts().then((_) {
-        setState(() {
-          _isLoading = false;
-        });
-      });
-    }
-    _isInit = false;
-    super.didChangeDependencies();
-  }
+class ProductOverviewScreen extends StatelessWidget {
+  final _showFavoriteOnly = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-        actions: [
-          Consumer<Cart>(
-            builder: (ctx, cart, cartConsumerChild) => Badge(
-              // since IconButton no need to rebuild (only value needs to rebuild),
-              // we can put IconButton in child of Consumer which is then pass via 3rd arguments
-              // `cartConsumerChild`. This widget here will NOT be rebuilt!
-              child: cartConsumerChild,
-              value: cart.itemCount.toString(),
-            ),
-            child: IconButton(
-              icon: Icon(
-                Icons.shopping_cart,
+        appBar: AppBar(
+          title: Text('MyShop'),
+          actions: [
+            Consumer<Cart>(
+              builder: (ctx, cart, cartConsumerChild) => Badge(
+                // since IconButton no need to rebuild (only value needs to rebuild),
+                // we can put IconButton in child of Consumer which is then pass via 3rd arguments
+                // `cartConsumerChild`. This widget here will NOT be rebuilt!
+                child: cartConsumerChild,
+                value: cart.itemCount.toString(),
               ),
-              onPressed: () {
-                Navigator.of(context).pushNamed(CartScreen.routeName);
-              },
+              child: IconButton(
+                icon: Icon(
+                  Icons.shopping_cart,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pushNamed(CartScreen.routeName);
+                },
+              ),
             ),
-          ),
-          PopupMenuButton(
-            onSelected: (DisplayOptions selectedValue) {
-              print('selectedValue..... $selectedValue');
+            PopupMenuButton(
+              onSelected: (DisplayOptions selectedValue) {
+                print('selectedValue..... $selectedValue');
 
-              setState(() {
-                if (selectedValue == DisplayOptions.ShowFavorites) {
-                  _showFavoriteOnly = true;
-                } else {
-                  _showFavoriteOnly = false;
-                }
-              });
-            },
-            icon: Icon(
-              Icons.more_vert,
-            ),
-            itemBuilder: (ctx) => [
-              PopupMenuItem(
-                child: Text('Show Favorites'),
-                value: DisplayOptions.ShowFavorites,
+                // setState(() {
+                //   if (selectedValue == DisplayOptions.ShowFavorites) {
+                //     _showFavoriteOnly = true;
+                //   } else {
+                //     _showFavoriteOnly = false;
+                //   }
+                // });
+              },
+              icon: Icon(
+                Icons.more_vert,
               ),
-              PopupMenuItem(
-                child: Text('Show All'),
-                value: DisplayOptions.ShowAll,
-              )
-            ],
-          ),
-        ],
-      ),
-      drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ProductsGrid(_showFavoriteOnly),
-    );
+              itemBuilder: (ctx) => [
+                PopupMenuItem(
+                  child: Text('Show Favorites'),
+                  value: DisplayOptions.ShowFavorites,
+                ),
+                PopupMenuItem(
+                  child: Text('Show All'),
+                  value: DisplayOptions.ShowAll,
+                )
+              ],
+            ),
+          ],
+        ),
+        drawer: AppDrawer(),
+        body: FutureBuilder(
+          future: Provider.of<Products>(context, listen: false).fetchProducts(),
+          builder: (ctx, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (dataSnapshot.error != null) {
+              return Center(child: Text('An error occurred!'));
+            } else {
+              return ProductsGrid(_showFavoriteOnly);
+            }
+          },
+        ));
   }
 }
